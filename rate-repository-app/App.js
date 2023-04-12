3082195587cdb5d68a767e911f378cfe746b8238
Anthony Collins <anthony.phillip.collins@gmail.com>
2023-04-12 13:37:48 +1000

    Exercise 10.10: a platform-specific font

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,8 +1,30 @@
 import Main from "./src/components/Main";
 import { NativeRouter } from "react-router-native";
 import { StatusBar } from "expo-status-bar";
+import * as Font from "expo-font";
+import { useEffect, useState } from "react";
+
+let customFonts = {
+  Roboto: require("./assets/fonts/Roboto-Regular.ttf"),
+  RobotoBlack: require("./assets/fonts/Roboto-Black.ttf"),
+};
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  const _loadFontsAsync = async () => {
+    await Font.loadAsync(customFonts);
+    setFontsLoaded(true);
+  };
+
+  useEffect(() => {
+    _loadFontsAsync();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <NativeRouter>
